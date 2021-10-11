2c7e527df722fcbb8d83bb8ebd29492f9e16986e
Jessica Lee <jesslee@cs.utexas.edu>
2021-10-11 13:57:39 -0500

    Added Tutorial and updated misc.

---
+++ ForestHopper v0.6 Add Tutorial State/js/game.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1058, 600, Phaser.AUTO, 'gameDiv');
+game.state.add('boot',bootState);
+
+game.state.add('load',loadscreen);
+
+game.state.add('mainmenu',menustate);
+
+game.state.add('gamestate',gamestate);
+
+game.state.add('tutorialstate', tutorialstate);
+
+console.log("states added");
+game.state.start('boot');
