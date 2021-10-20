4478fcf1caeb657a1cd222999652c144b474d0b7
Hjohny <90059899+Hjohny@users.noreply.github.com>
2021-10-20 02:54:30 -0500

    Add files via upload

---
+++ ForestHopper v0.8 overworld:boss-ish/js/game.js
@@ -0,0 +1,19 @@
+var game = new Phaser.Game(1058, 600, Phaser.AUTO, 'gameDiv');
+game.state.add('boot',bootState);
+
+game.state.add('load',loadscreen);
+
+game.state.add('mainmenu',menustate);
+
+game.state.add('gamestate',gamestate);
+
+game.state.add('tutorialstate', tutorialstate);
+
+game.state.add('level1', level1);
+
+game.state.add('overworldstate', overworldstate);
+
+game.state.add('level1Boss', level1Boss);
+
+console.log("states added");
+game.state.start('boot');
---
+++ ForestHopper v0.8 overworld:boss-ish/js/tutorialstate.js
@@ -0,0 +1,28 @@
+var tutorialstate = {
+    preload: function(){
+        game.load.image('bg_img', 'assets/TutorialBg.png');
+        game.load.image('howText', 'assets/HowToFont.png');
+        game.load.image('description', 'assets/description.png');
+        game.load.image('key', 'assets/movementKey.png');
+        game.load.image('cont', 'assets/Cont.png');
+    },
+    
+    create: function(){
+        bg = this.game.add.sprite(0,0, 'bg_img');
+        bg.height = game.height;
+        bg.width = game.width;
+        how = this.game.add.sprite(310, 100, 'howText');
+        des = this.game.add.sprite(220,200, 'description');
+        key = this.game.add.sprite(220, 300, 'key');
+        cont = this.game.add.sprite(900, 550, 'cont');
+        
+        var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        
+        enterKey.onDown.addOnce(this.play, this);
+    },
+    
+    play: function(){
+        game.state.start('gamestate');
+}
+
+}
